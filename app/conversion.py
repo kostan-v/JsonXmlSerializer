e80f4f2a015b73d10c2f81cfb8cab52a9ecc78fa
Viktor Košťan <viktor.kostan@gmail.com>
2021-06-29 18:02:59 +0200

    Resolve pylint issues (except for docstrings in test_main.py)

--- app/conversion.py
+++ app/conversion.py
@@ -1,3 +1,5 @@
+"""Implementation of encoding / decoding JSON to XML"""
+
 from xml.etree import ElementTree as ET
 from xml.dom import minidom
 import io
@@ -15,21 +17,20 @@ TYPE_MAPPING = {
     ("list", list),
 }
 
-KEYWORD_TO_PYTHON_TYPE = {type_kw: t for type_kw, t in TYPE_MAPPING}
+KEYWORD_TO_PYTHON_TYPE = dict(TYPE_MAPPING)
 PYTHON_TYPE_TO_KEYWORD = {t: type_kw for type_kw, t in TYPE_MAPPING}
 
 
 class InvalidXmlFormat(Exception):
     """Raised when parsed XML data in not valid XML file"""
-    pass
 
 
 class InvalidXmlItem(Exception):
     """Raised when parsing of <ITEM> element fails"""
-    pass
 
 
 def decode_json_item(item: ET.Element):
+    """Convert XML-encoded JSON element to Python object"""
     if item.tag != "ITEM":
         raise InvalidXmlItem(f"Unexpected element '{item.tag}'")
 
@@ -38,77 +39,77 @@ def decode_json_item(item: ET.Element):
     if item_type is None:
         raise InvalidXmlItem(f"Missing 'type' attribute in element '{item.text}'")
 
-    elif item_type == "list":
+    if item_type == "list":
         return [decode_json_item(child) for child in item]
 
-    elif item_type == "object":
+    if item_type == "object":
         for child in item:
             if "key" not in child:
                 InvalidXmlItem(f"Missing 'key' attribute in element '{child.text}'")
         return {child.attrib["key"]: decode_json_item(child) for child in item}
 
-    elif item_type == "null":
+    if item_type == "null":
         return None
 
-    elif item_type in KEYWORD_TO_PYTHON_TYPE:
+    if item_type in KEYWORD_TO_PYTHON_TYPE:
         value = item.get("value")
         if value is None:
             raise InvalidXmlItem(f"Missing 'value' attribute in element '{item.text}'")
         return KEYWORD_TO_PYTHON_TYPE[item_type](value)
 
-    else:
-        raise InvalidXmlItem(f"Unexpected 'type' attribute '{item_type}'")
+    raise InvalidXmlItem(f"Unexpected 'type' attribute '{item_type}'")
 
 
 def decode_json(xml_data: bytes):
-    """Convert XML-encoded JSON  to python object that can be converted to JSON using json.dumps"""
+    """Convert XML-encoded JSON to python object that can be converted to JSON using json.dumps"""
     stream = io.BytesIO(xml_data)
     try:
         tree = ET.parse(stream)
-    except ET.ParseError:
-        raise InvalidXmlFormat
+    except ET.ParseError as err:
+        raise InvalidXmlFormat from err
     root = tree.getroot()
     return decode_json_item(root)
 
 
 def encode_json_item(obj, parent=None) -> ET.Element:
+    """Convert Python object to XML-encoded JSON element"""
     type_kw = PYTHON_TYPE_TO_KEYWORD.get(type(obj))
     if type_kw is None:
         raise Exception(f"Unsupported python object of type {type(obj)} for conversion to JSON!")
 
     if parent is None:
-        el = ET.Element("ITEM")
+        node = ET.Element("ITEM")
     else:
-        el = ET.SubElement(parent, "ITEM")
-    el.attrib["type"] = type_kw
+        node = ET.SubElement(parent, "ITEM")
+    node.attrib["type"] = type_kw
 
     if obj is None:
         pass
     elif isinstance(obj, list):
-        for v in obj:
-            encode_json_item(v, el)
+        for val in obj:
+            encode_json_item(val, node)
     elif isinstance(obj, dict):
-        for k, v in obj.items():
-            child = encode_json_item(v, el)
-            child.attrib["key"] = str(k)
+        for key, val in obj.items():
+            child = encode_json_item(val, node)
+            child.attrib["key"] = str(key)
     elif isinstance(obj, bool):
-        el.attrib["value"] = "true" if obj else "false"
+        node.attrib["value"] = "true" if obj else "false"
     else:
-        el.attrib["value"] = str(obj)
+        node.attrib["value"] = str(obj)
 
-    return el
+    return node
 
 
-def prettyprint_xml(el : ET.Element):
+def prettyprint_xml(node : ET.Element):
+    """Get string containing prettyprinted XML element (with children)."""
     # Apparently if you want to prettyprint xml in native Python you have to either format and parse
     # and reformat XML using minidom but it cannot ommit header.
 
-    pretty_xml = minidom.parseString(ET.tostring(el)).toprettyxml(indent="    ")
+    pretty_xml = minidom.parseString(ET.tostring(node)).toprettyxml(indent="    ")
     return pretty_xml.split('\n', maxsplit=1)[1].rstrip('\n')
 
 
 def encode_json(obj) -> bytes:
     """Convert python object convertable to JSON to XML"""
-    el = encode_json_item(obj)
-    return prettyprint_xml(el)
-
+    root = encode_json_item(obj)
+    return prettyprint_xml(root)
--- app/main.py
+++ app/main.py
@@ -1,3 +1,5 @@
+"""Implementation of JsonXmlSerailizer server"""
+
 from pathlib import Path
 import os
 import json
@@ -16,16 +18,21 @@ app = FastAPI()
 
 @app.get("/")
 async def index_page():
+    """
+    Returns utility web app that uses the `/json2xml` and `/xml2json` endpoints to convert
+    between JSON and XML.
+    """
     page = INDEX_FILE_PATH.read_text()
     return Response(content=page, media_type="text/html")
 
 
 @app.post("/json2xml")
 async def json2xml(request: Request):
+    """Converts a POSTed JSON file to an XML file and returns it."""
     try:
         json_body = await request.json()
-    except json.decoder.JSONDecodeError:
-        raise HTTPException(status_code=400, detail="Data in body is not valid JSON")
+    except json.decoder.JSONDecodeError as err:
+        raise HTTPException(status_code=400, detail="Data in body is not valid JSON") from err
     print(json_body)
     data = encode_json(json_body)
     resp = Response(content=data, media_type="application/xml")
@@ -34,12 +41,13 @@ async def json2xml(request: Request):
 
 @app.post("/xml2json")
 async def xml2json(request: Request):
+    """Converts a POSTed XML file to a JSON and returns it."""
     raw_body = await request.body()
     print(raw_body)
     try:
         resp = decode_json(raw_body)
-    except InvalidXmlFormat:
-        raise HTTPException(status_code=400, detail="Data in body is not valid XML file")
-    except InvalidXmlItem as e:
-        raise HTTPException(status_code=400, detail=str(e))
+    except InvalidXmlFormat as err:
+        raise HTTPException(status_code=400, detail="Data in body is not valid XML file") from err
+    except InvalidXmlItem as err:
+        raise HTTPException(status_code=400, detail=str(err)) from err
     return resp
